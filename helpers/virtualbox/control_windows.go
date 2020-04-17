542cc97ceb0d2dc692180d34a11556162a1c87a9
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-04-17 16:26:45 +0200

    Code review

--- helpers/virtualbox/control_windows.go
+++ helpers/virtualbox/control_windows.go
@@ -1,22 +1,35 @@
 package virtualbox
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 
-	log "github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	// Add default virtualbox location to end of local PATH
-	for _, programFiles := range []string{os.Getenv("ProgramFiles"), os.Getenv("ProgramFiles(x86)"), os.Getenv("ProgramW6432")} {
-		if _, err := os.Stat(filepath.Join(programFiles, `Oracle\VirtualBox\VBoxManage.exe`)); err == nil {
-			virtualBoxPath := filepath.Join(programFiles, `Oracle\VirtualBox`)
-			path := os.Getenv("PATH")
-			path += ";" + virtualBoxPath
-			os.Setenv("PATH", path)
-			log.Debugln("Add autodetected VirtualBoxManage.exe to end of local PATH: ", virtualBoxPath)
-			break
-		}
+	_ = addDirectoryToPATH(os.Getenv("ProgramFiles"))
+	_ = addDirectoryToPATH(os.Getenv("ProgramW6432"))
+}
+
+func addDirectoryToPATH(programFilesPath string) error {
+	if programFilesPath == "" {
+		return errors.New("invalid ProgramFiles path")
 	}
+
+	virtualBoxPath := filepath.Join(programFilesPath, "Oracle", "VirtualBox")
+	newPath := fmt.Sprintf("%s;%s", os.Getenv("PATH"), virtualBoxPath)
+	err := os.Setenv("PATH", newPath)
+	if err != nil {
+		logrus.Warnf(
+			"Failed to add path to VBoxManage.exe (%q) to end of local PATH: %s",
+			virtualBoxPath,
+			err)
+		return err
+	}
+
+	logrus.Debugf("Added path to VBoxManage.exe to end of local PATH: %q", virtualBoxPath)
+	return nil
 }
